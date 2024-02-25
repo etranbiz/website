f74b4f7673be048b0203bddc266bbd4216bcb0f8
Aayanjit <aayan.work99@gmail.com>
2024-02-25 23:52:33 +0530

    Changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   description: 'eTranBiz is a IT Consulting Organization offering High-End Expertise, Solutions and Services based on Oracle Products and Technologies especially in the areas of Oracle Fusion Middleware, Oracle eBusiness Suite, Oracle Database and Enterprise Systems, Data warehouse and BI and Java/Web/Mobile Solution Development Technologies.',
   // icons: [{ rel: 'icon', url: Favicon.src }],
   icons: {
-    icon: ['/etranbiz-favicon.ico'],
+    icon: ['/etranbiz-favicon 1.ico'],
   },
 }
 
