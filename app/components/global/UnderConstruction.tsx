dc4b98568c0d2b0f1d24e0b8d1bab68dfa2142da
Aayanjit <aayan.work99@gmail.com>
2024-02-28 19:45:13 +0530

    Updated email id for contact point

--- app/components/global/UnderConstruction.tsx
+++ app/components/global/UnderConstruction.tsx
@@ -9,7 +9,7 @@ import logo from '@/app/assets/icons/logo-main-white.svg'
 const UnderConstruction = () => {
 
     const handleButtonClick = () => {
-        window.location.href = 'mailto:sales@etranbiz.com';
+        window.location.href = 'mailto:info@etranbiz.com';
       };
 
     return (
